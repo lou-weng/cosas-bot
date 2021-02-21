1dda482fd00f711c2f60e65ec3b480e0bbf0ff0e
lou-weng <louieweng@outlook.com>
2021-02-20 23:43:14 -0500

    updated requirements

--- slackbot.py
+++ slackbot.py
@@ -9,11 +9,14 @@ dotenv.load_dotenv()
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events')
 
 client = WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='test-bot', text="hello again")
+
+BOT_ID = client.api_call("auth.test")['user_id']
+
 @slack_event_adapter.on('message')
 def message(payload):
     print("Hello")
     text = "Hello there!"
-    client.chat_postMessage(channel='test-bot', text=text)
+    if BOT_ID != payload.get('event', {}).get('user'):
+        client.chat_postMessage(channel='test-bot', text=text)
 
 slack_event_adapter.start(port=5000)
